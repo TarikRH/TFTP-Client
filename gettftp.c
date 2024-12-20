7f551b7063bf3de67497f5cf908763233aa255de
ensea <ensea@StudentLab>
2024-12-20 16:20:31 +0100

    Update

--- gettftp.c
+++ gettftp.c
@@ -6,30 +6,37 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include "gettftp.h"
+#include <unistd.h>
 
-#define RRQ_OPCODE 2
+#define RRQ_OPCODE 1
 #define SIZE 512
 
-int getftftp(char *server, char *file, struct addrinfo *res){
-    unsigned  char buf[SIZE];
-    int offset = 0;
+int getftftp(char *server, char *file, struct addrinfo *res) {
+    unsigned char buf[SIZE]; 
+    int offset = 0;          // Offset to write data in the buffer
+
+    // Creating a socket
     int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
+    // Add the TFTP opcode for RRQ to the buffer
     buf[offset++] = 0x00;      
     buf[offset++] = RRQ_OPCODE;
 
+
+    // Add the requested file name to the buffer
     strcpy((char *)&buf[offset], file);
-    offset += strlen(file) + 1; 
+    offset += strlen(file) + 1;
 
+    // Add the transfer mode "octet" to the buffer
     strcpy((char *)&buf[offset], "octet");
-    offset += strlen("octet") + 1; 
+    offset += strlen("octet") + 1;
 
+    
+    ssize_t sent_bytes = sendto(sock, buf, offset, 0, res->ai_addr, res->ai_addrlen); // Send the TFTP request to the server
 
-    ssize_t sent_bytes = sendto(sock,buf,offset,0,res->ai_addr,res->ai_addrlen);
-    if (sent_bytes==-1){
-
+    if (sent_bytes == -1) { // If there is an error while sending the request, then return -1
+        
         return -1;
     }
-
     return sock;
 }
--- gettftp.h
+++ gettftp.h
@@ -1,12 +1,6 @@
 #ifndef GETTFTP_H
 #define GETTFTP_H
 
-#define RRQ_OPCODE 2  
-#define SIZE 512
-
-#include <sys/socket.h>
-#include <netdb.h>
-
-int getftftp(char *server, char *file, struct addrinfo *res);
+int gettftp(char *server, char *file, struct addrinfo *res);
 
 #endif
